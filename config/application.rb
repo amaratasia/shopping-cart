648c02d2f1c807752663099621ef8bd3292b3f52
Ananth Amar <ananth.amar@sleekr.co>
2020-09-14 22:48:45 +0700

    Auto Load Presenter

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module ShoppingCart
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.autoload_paths += %W(
+      #{Rails.root}/app/models/presenters
+    )
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
