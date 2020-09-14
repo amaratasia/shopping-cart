8bf93c80f703fd63a24acd1912ec93be206b9bf7
Ananth Amar <ananth.amar@sleekr.co>
2020-09-14 22:49:32 +0700

    Adding Association

--- app/models/promotion_category.rb
+++ app/models/promotion_category.rb
@@ -1,5 +1,6 @@
 class PromotionCategory < ActiveRecord::Base
   enum promotion_type: {n_for_m: 1, cart_logic: 2}
+  has_many :product_promotions
 
   validates :promotion_type, :rules, presence: true
 
